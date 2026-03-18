b9beaf895b181e4ecc04113405b35147a3865e86
dmn <dominik.jalc@gmail.com>
2026-03-18 20:46:59 +0000

    chore(nvim): remove unused plugins

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,4 @@
 return {
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   lazy = true,
-  --   opts = { style = "night" },
-  -- opts = { style = "day" },
-  -- },
-  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   { "miikanissi/modus-themes.nvim", priority = 1000 },
   {
     "LazyVim/LazyVim",
